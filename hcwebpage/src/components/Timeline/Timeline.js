14029f1e95f583464ce33ff16a7a596337d88349
nghochi123 <nghochi123@gmail.com>
2021-06-28 01:30:39 +0800

    Some modifications

--- hcwebpage/src/components/Timeline/Timeline.js
+++ hcwebpage/src/components/Timeline/Timeline.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import TimelineItem from './TimelineItem/TimelineItem';
+import TimelineItem from "./TimelineItem/TimelineItem";
 
-
-import styles from './Timeline.module.css'
+import styles from "./Timeline.module.css";
 
 const Timeline = (props) => {
-    return (
-        <div className={styles.timeline}>
-            <ul>
-                <TimelineItem title="Data Analyst Intern" time="Dec 2020" child={1}>Interned at MOH as a Data Analyst for 3 months, handling information related to COVID operations. My team primarily uses Stata in our operations</TimelineItem>
-                <TimelineItem title="National Service" time="2019-2020" child={2}>Served the nation for two years. Not the best time of my life, but happy to have been able to contribute. Started taking some online courses that interested me during my free time in NS.</TimelineItem>
-                <TimelineItem title="Graduation from HCI" time="Oct 2018" child={1}>Graduated from HCI in October 2018, with the only thing left being A-levels</TimelineItem>
-                <TimelineItem title="RoboCup Montreal" time="Jun 2018" child={2}>After clinching the championship title in Singapore, my team was offered an opportunity to represent Singapore in an international Robotics competition.</TimelineItem>
-                <TimelineItem title="Enrolled in HCI" time="Jan 2017" child={1}>Got into HCI after doing well for my O-levels, and managed to join HCIRS, where my interest in building and creating developed.</TimelineItem>
-                <div className={styles.clearBoth}></div>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className={styles.timeline}>
+      <ul>
+        <TimelineItem title="Data Analyst Intern" time="Dec 2020" child={1}>
+          Interned at MOH as a Data Analyst for 3 months, handling information
+          related to COVID operations. My team primarily uses Stata in our
+          operations
+        </TimelineItem>
+        <TimelineItem title="National Service" time="2019-2020" child={2}>
+          Served the nation for two years. Not the best time of my life, but
+          happy to have been able to contribute. Started taking some online
+          courses that interested me during my free time in NS.
+        </TimelineItem>
+        <TimelineItem title="Graduation from HCI" time="Oct 2018" child={1}>
+          Graduated from HCI in October 2018, with the only thing left being
+          A-levels
+        </TimelineItem>
+        <TimelineItem title="RoboCup Montreal" time="Jun 2018" child={2}>
+          After clinching the championship title in Singapore, my team was
+          offered an opportunity to represent Singapore in an international
+          Robotics competition.
+        </TimelineItem>
+        <TimelineItem title="Enrolled in HCI" time="Jan 2017" child={1}>
+          Got into HCI after doing well for my O-levels, and managed to join
+          HCIRS, where my interest in building and creating developed.
+        </TimelineItem>
+        <div className={styles.clearBoth}></div>
+      </ul>
+    </div>
+  );
+};
 
 export default Timeline;
-
--- hcwebpage/src/containers/Links/Links.js
+++ hcwebpage/src/containers/Links/Links.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import { Container, Col, Row } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faBook } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Container, Col, Row } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faBook } from "@fortawesome/free-solid-svg-icons";
 
-import Layout from '../../components/Layout/Layout';
+import Layout from "../../components/Layout/Layout";
 
-import styles from './Links.module.css';
+import styles from "./Links.module.css";
 
-
-
-const Projects = props => {
-    return (
-        <Layout>
-            <h1 className={styles.submaintext}>Links</h1>
-            <Container className={styles.cont}>
-                <Row>
-                    <Col className={styles.col}><a style={{color: '#000'}} href="https://github.com/nghochi123"><FontAwesomeIcon className={`${styles.icon} ${styles.github}`} icon={faGithub} /><p>GitHub</p></a></Col>
-                    <Col className={styles.col}><a style={{color: '#000'}} href="mailto:nghochi123@gmail.com"><FontAwesomeIcon className={`${styles.icon} ${styles.mail}`} icon={faEnvelope} /><p>Email</p></a></Col>
-                    <Col className={styles.col}><a style={{color: '#000'}} href="https://www.linkedin.com/in/ng-ho-chi-809a881b9/"><FontAwesomeIcon className={`${styles.icon} ${styles.linkedin}`} icon={faLinkedin} /><p>Linkedin</p></a></Col>
-                    <Col className={styles.col}><a style={{color: '#000'}} href="https://nghochi-blog.netlify.app/"><FontAwesomeIcon className={`${styles.icon} ${styles.mail}`} icon={faBook} /><p>Blog</p></a></Col>
-                </Row>
-            </Container>
-        </Layout>
-    )
-    }
+const Projects = (props) => {
+  return (
+    <Layout>
+      <h1 className={styles.submaintext}>Links</h1>
+      <Container className={styles.cont}>
+        <Row>
+          <Col className={styles.col}>
+            <a style={{ color: "#000" }} href="https://github.com/nghochi123">
+              <FontAwesomeIcon
+                className={`${styles.icon} ${styles.github}`}
+                icon={faGithub}
+              />
+              <p>GitHub</p>
+            </a>
+          </Col>
+          <Col className={styles.col}>
+            <a style={{ color: "#000" }} href="mailto:nghochi123@gmail.com">
+              <FontAwesomeIcon
+                className={`${styles.icon} ${styles.mail}`}
+                icon={faEnvelope}
+              />
+              <p>Email</p>
+            </a>
+          </Col>
+          <Col className={styles.col}>
+            <a
+              style={{ color: "#000" }}
+              href="https://www.linkedin.com/in/ng-ho-chi-809a881b9/"
+            >
+              <FontAwesomeIcon
+                className={`${styles.icon} ${styles.linkedin}`}
+                icon={faLinkedin}
+              />
+              <p>Linkedin</p>
+            </a>
+          </Col>
+          <Col className={styles.col}>
+            <a style={{ color: "#000" }} href="https://hochi-blog.netlify.app/">
+              <FontAwesomeIcon
+                className={`${styles.icon} ${styles.mail}`}
+                icon={faBook}
+              />
+              <p>Blog</p>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 export default Projects;
